1365892ab64ce95b171771c6c3fa4b76e0bd2057
antho1404 <anthony.estebe@gmail.com>
2020-06-08 17:56:38 +0700

    migrate to Liteflow

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Service = require('@mesg/service')
+const Service = require('@liteflow/service')
 const pretrainedModels = require('./tasks/pretrainedModels')
 // tf.loadGraphModel("https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v1_050_160/classification/1/default/1", { fromTFHub: true })
-const mesg = new Service()
+const liteflow = new Service()
 // const readImage = path => {
 //   const imageBuffer = fs.readFileSync(path);
 //   const tfimage = tfnode.node.decodeImage(imageBuffer);
@@ -10,13 +10,13 @@ const mesg = new Service()
 // загрузить модель
 async function main(){
 
-  mesg.listenTask({
+  liteflow.listenTask({
     predict: require('./tasks/predict'),
     mobilenet_classify : pretrainedModels.mobilenet_classify,
   })
   .on('error', (error) => console.error(error))
   
-  mesg.emitEvent('started', { x: true })
+  liteflow.emitEvent('started', { x: true })
     .catch((error) => console.error(error))
 }
 
